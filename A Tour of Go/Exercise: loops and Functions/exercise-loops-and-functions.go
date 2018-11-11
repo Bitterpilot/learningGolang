9d015718830de67065f7cbcb1f78fff59120419c
Nathan Minissale <nathanminissale@gmail.com>
2018-11-11 12:01:13 +0800

    Exercise from https://tour.golang.org/flowcontrol/8 Used newtons method from the exercise in for loop

--- A Tour of Go/Exercise: loops and Functions/exercise-loops-and-functions.go
+++ A Tour of Go/Exercise: loops and Functions/exercise-loops-and-functions.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"math"
 	"fmt"
 )
 
 // Sqrt compute the square root of x using a loop
 func Sqrt(x float64) float64 {
-
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Println("iteration:", i, " Result:", z)
+	}
+	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	x := 2.0
+	fmt.Println(math.Sqrt(x), Sqrt(x))
 }
