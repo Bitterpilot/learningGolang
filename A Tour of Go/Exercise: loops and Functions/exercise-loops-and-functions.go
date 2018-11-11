30bf771376fb4f7f7c6c20bdce4ea447f9e5ad45
Nathan Minissale <nathanminissale@gmail.com>
2018-11-11 12:19:18 +0800

    Exercise from https://tour.golang.org/flowcontrol/8 Changed z := float64() as a way to document that z must be a float
    changed Prints for a more concise expression

--- A Tour of Go/Exercise: loops and Functions/exercise-loops-and-functions.go
+++ A Tour of Go/Exercise: loops and Functions/exercise-loops-and-functions.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"math"
 	"fmt"
+	"math"
 )
 
 // Sqrt compute the square root of x using a loop
 func Sqrt(x float64) float64 {
-	z := 1.0
+	z := float64(1)
 	for i := 0; i < 10; i++ {
 		z -= (z*z - x) / (2 * z)
-		fmt.Println("iteration:", i, " Result:", z)
+		fmt.Printf("iteration: %d Result: %g\n", i, z)
 	}
 	return z
 }
 
 func main() {
 	x := 2.0
-	fmt.Println(math.Sqrt(x), Sqrt(x))
+	fmt.Printf("Standard lib: %g\n     My Sqrt: %g\n", math.Sqrt(x), Sqrt(x))
 }
