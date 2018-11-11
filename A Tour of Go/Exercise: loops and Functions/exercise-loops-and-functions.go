82211d9fab258ea774d5d7f7468c97a7c2168f95
Nathan Minissale <nathanminissale@gmail.com>
2018-11-11 12:59:50 +0800

    Exercise from https://tour.golang.org/flowcontrol/8 Exercise complete my Sqrt function breaks if the resault is within a precision threashold

--- A Tour of Go/Exercise: loops and Functions/exercise-loops-and-functions.go
+++ A Tour of Go/Exercise: loops and Functions/exercise-loops-and-functions.go
@@ -7,15 +7,20 @@ import (
 
 // Sqrt compute the square root of x using a loop
 func Sqrt(x float64) float64 {
+	const precision = 0.00000001
 	z := float64(1)
+	y := float64(0)
 	for i := 0; i < 10; i++ {
+		y = z
 		z -= (z*z - x) / (2 * z)
-		fmt.Printf("iteration: %d Result: %g\n", i, z)
+		fmt.Printf("iteration: %d Result: %g and y= %g\n", i, z, y)
+		// If equal within 8 decimal places
+		if diff := math.Abs(z - y); diff < precision {break}
 	}
 	return z
 }
 
 func main() {
-	x := 2.0
+	x := 16.55445
 	fmt.Printf("Standard lib: %g\n     My Sqrt: %g\n", math.Sqrt(x), Sqrt(x))
 }
