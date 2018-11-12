98e90275537508bbe2e130884d117fa9e2a1fe07
Nathan Minissale <nathanminissale@gmail.com>
2018-11-12 11:22:21 +0800

    Get Xml from w3schools.com

--- Http/main.go
+++ Http/main.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/about", aboutHandler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hay, Ho,")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Lets Go!")
+	// http.Get returns 2 things. resp and err,
+	// we don't need error in this case so we igore it with ", _"
+	resp, _ := http.Get("https://www.w3schools.com/xml/simple.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	stringBody := string(bytes)
+	fmt.Println(stringBody)
+	resp.Body.Close()
 }
