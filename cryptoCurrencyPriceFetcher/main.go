f49726976659c78abd98c44f5d27b0cc3de4de35
Nathan Minissale <nathanminissale@gmail.com>
2018-11-12 19:27:28 +0800

    Successfully reading json from local file

--- cryptoCurrencyPriceFetcher/main.go
+++ cryptoCurrencyPriceFetcher/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main()  {
-	
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+// Coins struct which contains an array of data
+type Coins struct {
+	Coins []Coin `json:"data"`
+}
+
+// Coin struct which contains the id, name, symbol
+// and slug of a coin
+type Coin struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Symbol      string `json:"symbol"`
+	WebsiteSlug string `json:"website_slug"`
+}
+
+func main() {
+	jsonFile, _ := os.Open("sample.json")
+	defer jsonFile.Close()
+
+
+	fmt.Println("Successfully Opened sample.json")
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var coins Coins
+
+	json.Unmarshal(byteValue, &coins)
+
+	for i := 0; i < len(coins.Coins); i++ {
+		fmt.Println("ID: " + strconv.Itoa(coins.Coins[i].Id))
+		fmt.Println("Symbol: " + coins.Coins[i].Symbol)
+		fmt.Println("Name: " + coins.Coins[i].Name)
+		fmt.Println("slug: " + coins.Coins[i].WebsiteSlug)
+	}
 }
