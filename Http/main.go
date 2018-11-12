5d527cd3159089d01567bb6cf580a52a7df14220
Nathan Minissale <nathanminissale@gmail.com>
2018-11-12 11:34:41 +0800

    Extract the name of each food item from the xml and place in array

--- Http/main.go
+++ Http/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+
+type breakfastMenu struct {
+	Name []name `xml:"food"`
+}
+
+type name struct {
+	Nam string `xml:"name"`
+}
 
 func main() {
 	// http.Get returns 2 things. resp and err,
 	// we don't need error in this case so we igore it with ", _"
 	resp, _ := http.Get("https://www.w3schools.com/xml/simple.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	stringBody := string(bytes)
-	fmt.Println(stringBody)
 	resp.Body.Close()
+
+	var s breakfastMenu
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Name)
 }
